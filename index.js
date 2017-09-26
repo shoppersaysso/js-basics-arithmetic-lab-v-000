eb6041bf7417e16314e4f554b5dd85e0926a062a
Sara Hopper <sara.hopper@gmail.com>
2017-09-26 21:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ let newID = oldID + 1000000000;
 
 let ageIsValid =  Number.isInteger(currentAge);
 
-let randomNumber = Math.floor(Math.random() * 10 * 2);
+let randomNumber = Math.random() * 20;
 
 let randomInteger = Math.floor(randomNumber);
